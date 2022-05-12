e103fa2ba4662cd0d338b3b509c658148036ff85
nguyenvanhieu <hieunvph15760@fpt.edu.vn>
2022-05-12 22:46:42 +0700

    Up project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular_we16304';
+  title = 'angular_we16304  hahahahaha';
+
+  teachers = [
+    {
+      id:1,
+      name:"Hoa",
+      age:20,
+      gender:0,
+      avatar:"https://img.lovepik.com/photo/50112/2990.jpg_wh860.jpg",
+      status:1
+    },
+    {
+      id:2,
+      name:"Hiếu",
+      age:20,
+      gender:1,
+      avatar:"https://cdn.luatvietnam.vn/uploaded/Images/Original/2020/11/17/so-sach-cua-giao-vien-cac-cap_1711143350.jpg",
+      status:0
+    }
+  ]
 }
---
+++ src/app/table/table-gender/table-gender.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-gender',
+  templateUrl: './table-gender.component.html',
+  styleUrls: ['./table-gender.component.css']
+})
+export class TableGenderComponent implements OnInit {
+
+  @Input() gender:number;
+
+  constructor() {
+    this.gender = 0;
+   }
+
+  ngOnInit(): void {
+  }
+
+}
